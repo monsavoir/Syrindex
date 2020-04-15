3ed6b622fa0b3a5cef777f83fcd41e13941d46e3
MONSAVOIR <antoine@monsavoir.net>
2020-04-15 02:32:03 +0200

    retrieveDatabase + hasNumber function added

--- parsing.py
+++ parsing.py
@@ -1,11 +1,17 @@
 #!/usr/bin/env python3
+import requests
 
 def extendContinent(continent) :
-	file = open(continent, 'r')
-	countries = []
-	for line in file:
-		countries.append(line[:-1])
-	file.close()
-	return countries
+    file = open(continent, 'r')
+    countries = []
+    for line in file:
+        countries.append(line[:-1])
+    file.close()
+    return countries
 
-print(extendContinent('africa'), len(extendContinent('africa')))
+def hasNumber(s):
+    return any(char.isdigit() for char in s)
+
+def retrieveDatabase(query):
+    response = requests.get("https://www.xeno-canto.org/api/2/recordings?query="+query)
+    return response.json()['numRecordings']
